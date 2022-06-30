6e482558658a20a897636a4179d2ab39ca81c331
張志瑋 <proxy0001@zhangzhiweide-MacBook-Pro.local>
2022-06-30 13:35:47 +0800

    add mail

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,15 @@
 
 import styles from 'styles/Home.module.css';
 import {FaTwitter} from 'react-icons/fa';
+import {AiOutlineMail} from 'react-icons/ai';
 
 export default function Footer () {
   return (
     <footer className={styles.footer}>
       <div className={styles.links}>
+        <a href = "mailto:g.proxy0001@gmail.com">
+          <AiOutlineMail className={styles.linkIcon} />
+        </a>
         <a href="https://twitter.com/proxy0001" target="_blank" rel="noreferrer">
           <FaTwitter className={styles.linkIcon} />
         </a>
