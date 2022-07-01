c9d60f45356d5f48d0fd33c4a5b5eeebb119d45a
proxy0001 <g.proxy0001@gmail.com>
2022-07-01 13:09:59 +0800

    add github link at footer

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,20 @@
 
 import styles from 'styles/Home.module.css';
-import {FaTwitter} from 'react-icons/fa';
-import {AiOutlineMail} from 'react-icons/ai';
+import {FaTwitter, FaGithub} from 'react-icons/fa';
+import {MdEmail} from 'react-icons/md';
 
 export default function Footer () {
   return (
     <footer className={styles.footer}>
       <div className={styles.links}>
         <a href = "mailto:g.proxy0001@gmail.com">
-          <AiOutlineMail className={styles.linkIcon} />
+          <MdEmail className={styles.linkIcon} />
         </a>
-        <a href="https://twitter.com/proxy0001" target="_blank" rel="noreferrer">
+        <a href="https://twitter.com/proxy0001" target="_blank" rel="noreferrer noopener">
           <FaTwitter className={styles.linkIcon} />
+        </a>
+        <a href = "https://github.com/proxy0001" target="_blank" rel="noreferrer noopener">
+          <FaGithub className={styles.linkIcon} />
         </a>
       </div>
     </footer>
