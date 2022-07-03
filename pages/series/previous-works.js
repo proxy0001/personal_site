213ade2301083628922c881b35247a5d781ccf9a
proxy0001 <g.proxy0001@gmail.com>
2022-07-03 23:26:36 +0800

    update description and naming

--- pages/series/previous-works.js
+++
@@ -1,8 +0,0 @@
-import LayoutSeriesPage from 'components/layout-series-page';
-import { env, seriesPreviousWorks as seriesData } from 'store/state';
-
-export default function SeriesPreviousWorks() {
-  return (
-    <LayoutSeriesPage {...seriesData} url={env.url} />
-  )
-}
